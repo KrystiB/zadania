f787066d43aff7981db38e509846c3372ab12eee
krystibog <krystibog@gmail.com>
2024-03-29 15:32:12 +0100

    .

--- React/Rozdzial 2 - komponenty rozszerzone/zad-1/src/App.jsx
+++ React/Rozdzial 2 - komponenty rozszerzone/zad-1/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useImmer } from 'use-immer';
 import './App.css';
 
 const initialData = {
@@ -11,33 +12,40 @@ const initialData = {
 
 function App() {
     const [{ human, accountBalance }, setData] = useState(initialData);
+    const [person, updatePerson] = useImmer(initialData);
+
+    // const handleSexChange = () => {
+    //     setData((previousData) => ({
+    //         ...previousData,
+    //         human: {
+    //             ...previousData.human,
+    //             sex: previousData.human.sex === 'man' ? 'woman' : 'man',
+    //         },
+    //     }));
+    // };
 
     const handleSexChange = () => {
-        setData((previousData) => ({
-            ...previousData,
-            human: {
-                ...previousData.human,
-                sex: previousData.human.sex === 'man' ? 'woman' : 'man',
-            },
-        }));
+        updatePerson(draft => {
+            draft.human.sex = draft.human.sex === 'man' ? 'woman' : 'man';
+        });
     };
     const handleBuy = () => {
         setData((previousData) => ({
             ...previousData,
-            accountBalance: previousData.accountBalance-500,
+            accountBalance: previousData.accountBalance - 500,
         }));
     };
 
     const handleSell = () => {
         setData((previousData) => ({
-            ...previousData, 
-            accountBalance: previousData.accountBalance+500,
-        }))
-    }
+            ...previousData,
+            accountBalance: previousData.accountBalance + 500,
+        }));
+    };
 
     return (
         <>
-            <p>Płeć: {human.sex}</p>
+            <p>Płeć: {person.human.sex}</p>
             <p>Nazwisko: {human.surname}</p>
             <p>Bilans Konta: {accountBalance}</p>
             <button onClick={handleSexChange}>Toggle Sex</button>
