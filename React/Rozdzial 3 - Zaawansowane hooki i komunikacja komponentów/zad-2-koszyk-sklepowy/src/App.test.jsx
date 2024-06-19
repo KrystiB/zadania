8d7f67f4ac9c3ece6b3f691df7b71009c19621ca
krystibog <krystibog@gmail.com>
2024-06-19 17:11:46 +0200

    .

--- React/Rozdzial 3 - Zaawansowane hooki i komunikacja komponentów/zad-2-koszyk-sklepowy/src/App.test.jsx
+++ React/Rozdzial 3 - Zaawansowane hooki i komunikacja komponentów/zad-2-koszyk-sklepowy/src/App.test.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom/vitest'
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('Shopping Cart', () => {
-    test('renders initial cart state', () => {
+    it('renders initial cart state', () => {
         render(<App />);
         expect(screen.getByText('Koszyk sklepowy')).toBeInTheDocument();
         expect(
@@ -11,4 +13,16 @@ describe('Shopping Cart', () => {
         ).toBeInTheDocument();
         expect(screen.getByText('Suma wartości w koszyku: 10 zł')).toBeInTheDocument();
     });
+    it('adds an item to the cart', () => {
+        render(<App/>);
+
+        fireEvent.change(screen.getByPlaceholderText('Nazwa przedmiotu'), {target: {value: 'Jabłko'}})
+        fireEvent.change(screen.getByPlaceholderText('Cena produktu'), {target: {value: '5'}})
+        fireEvent.change(screen.getByPlaceholderText('Ilość produktu'), {target: {value: '3'}})
+
+        fireEvent.click(screen.getByText('Dodaj przedmiot'));
+
+        expect(screen.getByText('Jabłko - Cena: 5 zł - Ilość: 3 - Suma: 15')).toBeInTheDocument();
+        expect(screen.getByText('Suma wartości w koszyku: 25 zł')).toBeInTheDocument();
+    })
 });
--- React/Rozdzial 3 - Zaawansowane hooki i komunikacja komponentów/zad-2-koszyk-sklepowy/vite.config.js
+++ React/Rozdzial 3 - Zaawansowane hooki i komunikacja komponentów/zad-2-koszyk-sklepowy/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true, 
+    setupFiles: '../zad-2-koszyk-sklepowy/src/App.test.jsx'
+  }
 })
